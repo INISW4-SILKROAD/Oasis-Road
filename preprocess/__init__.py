448d3a47e49104cf8bb3818a74faadedee8a0b3d
esillileu <108880440+esillileu@users.noreply.github.com>
2024-06-17 17:51:08 +0900

    Update __init__.py

--- preprocess/__init__.py
+++ preprocess/__init__.py
@@ -1,9 +1,1 @@
-import os, sys
 
-sys.path.insert(0, os.path.join(sys.path[-1], 'CO-DETR'))
-print(sys.path)
-
-from .portion_regex import PortionRegex
-from .info_ocr import ocr
-from .custom_logger import get_logger
-from .crop_fabric import FabricCropper
